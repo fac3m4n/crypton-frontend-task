7be7a17a700583065e7a75aa993c01105a4d3476
fac3m4n <akerimberdi@gmail.com>
2025-01-22 11:49:56 +0200

    adding public route

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,32 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { ThemeProvider } from "./components/ThemeProvider";
 import MainLayout from "./components/layouts/MainLayout";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <MainLayout>
           <Routes>
-            {/* Public routes */}
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            {/* Public routes - redirect to profile if already logged in */}
+            <Route element={<PublicRoute />}>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Route>
 
             {/* Protected routes */}
             <Route element={<ProtectedRoute />}>
               <Route path="/profile" element={<Profile />} />
             </Route>
 
-            {/* Default route */}
-            <Route path="/" element={<Navigate to="/login" />} />
+            {/* Default route - redirect to profile if token exists, otherwise to login */}
+            <Route
+              path="/"
+              element={<Navigate to={token ? "/profile" : "/login"} replace />}
+            />
           </Routes>
         </MainLayout>
       </ThemeProvider>
