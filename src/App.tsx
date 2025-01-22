bf7519c5965177dd0df36e7b05c2704a63c93cdd
fac3m4n <akerimberdi@gmail.com>
2025-01-22 03:07:22 +0200

    remove useeffect for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import { Register, Login, Profile } from "@/pages";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { useEffect } from "react";
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      navigate("/profile");
-    }
-  }, [navigate]);
-
   return (
     <BrowserRouter>
       <Routes>
