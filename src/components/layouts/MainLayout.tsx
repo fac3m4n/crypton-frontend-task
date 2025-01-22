613b10021a05eee924bbbb10d399390342301b69
fac3m4n <akerimberdi@gmail.com>
2025-01-22 10:48:41 +0200

    them toggle fix and page dimplyfy

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import { ThemeToggle } from "../ThemeToggle";
+import { GalleryVerticalEnd } from "lucide-react";
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div>
-      <header className="flex justify-between p-4 border-b">
+    <div className="bg-background relative">
+      {/* Fixed Header */}
+      <div className="fixed top-0 left-0 right-0 z-50 flex justify-between p-4 border-b bg-background">
         <Link to="/">
-          <h1>My App</h1>
+          <div className="flex items-center gap-2 self-center font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            <div>Crypton</div>
+          </div>
         </Link>
         <ThemeToggle />
-      </header>
+      </div>
 
       {/* Page Content */}
-      <main>{children}</main>
+      <main className="pt-16">{children}</main>
     </div>
   );
 };
