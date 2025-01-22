4c5d776e111c8c854435fc6f32c045f1c6782e4b
fac3m4n <akerimberdi@gmail.com>
2025-01-22 02:05:28 +0200

    pages and routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { Register, Login, Profile } from "@/pages";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Button>Hello World</Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
